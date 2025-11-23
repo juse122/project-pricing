59f963ce32fef465f0ae856acd7a05fdc0646f23
juse122 <juse122de@gmail.com>
2025-11-23 11:30:11 +0100

    quick save

--- src/variables/variables.ts
+++ src/variables/variables.ts
@@ -2,8 +2,13 @@
 export const VAT = 0.19
 
 // DHL services and surcharges 2025
-export const ONE_KILO = 4.8
-export const THREE_KILO = 5.25
+export const ONE_KILO = 4.60
+export const THREE_KILO = 5.15
+export const FIVE_KILO = 6.20
+export const TEN_KILO = 8.25
+export const TWENTY_KILO = 13.15
+export const MAX_KILO = 15.90
+
 export const SMALL_PARCEL = 3.19
 export const RETURN = 6.19
 
@@ -16,27 +21,28 @@ export const ROUTING_SMALL = 1.49
 export const ENERGY_SURCHARGE = 1.0125
 export const EMISSION_SURCHARGE = 0.19
 export const PEAK_SURCHARGE = 0.19 // November and December
+export const PEAK_IN_PEAK_SURCHARGE = 0.50 // The 2 weeks surrounding Black Friday and Cyber Monday
 
 export const platformData: Platform[] = [
   {
     id: 0,
     name: "OTTO Market",
-    percentageProvision: 15.0,
-    flatProvision: 0,
+    percentageProvision: 15.00,
+    flatProvision: 0.00,
     shippingCost: 6.99
   },
   {
     id: 1,
     name: "Kaufland",
-    percentageProvision: 13.0,
-    flatProvision: 0,
+    percentageProvision: 13.00,
+    flatProvision: 0.00,
     shippingCost: 6.99
   },
   {
     id: 2,
     name: "Cardmarket",
-    percentageProvision: 4.2,
-    flatProvision: 0,
+    percentageProvision: 4.20,
+    flatProvision: 0.00,
     shippingCost: 7.19
   }
 ]
